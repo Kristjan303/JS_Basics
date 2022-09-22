2b4031cde65bbc127a35e85592d26c52d0205d92
Kristjan-Even <kristjan.puusepp@voco.ee>
2022-09-22 13:56:42 +0300

    while, do-while, for loops

--- app.js
+++ app.js
@@ -1,80 +1,39 @@
-/*
-if(condition) {
-    //if statements
-} else{
-    // else statements
+console.log("for loop")
+for (let number = 0; number < 10; number++) {
+    console.log(number)
 }
 
-== equal ===
-!= not equal !==
+//continue
+//break
 
+console.log("While loop")
 
+let number1 = 0 ;
+while (number1 < 10){
+    console.log(number1)
+    number1++
+}
 
- */
+console.log("do-while loop")
+let number2 = 11
+do{
+    console.log(number2)
+    number2++
+}while (number2 < 10)
 
-// let person_id = 34;
-//
-// if(person_id === "undefined"){
-//     console.log("id pähh pähh")
-// } else{
-//     console.log(`id is $(person_id)`)
-// }
+const cars  = ["audi", "tesla", "BMW"]
+cars.forEach((car) => {
+    console.log(car)
+})
 
-/*
-if(condition) {
-    //if statements
-} else{
-    // else statements
+const person = {
+    firstname: "kristjan",
+    surname: "puusepp",
+    age: "80"
+}
+
+for (let personKey in person) {
+    console.log(person[personKey])
 }
 
 
- */
-
-
-// const color = "red"
-//
-// if(color === "red") {
-//     console.log("go")
-//
-// }else if (color === "yellow") {
-//     console.log("stop")
-// } else {
-//     console.log("noob")
-// }
-//
-// console.log(person_id)
-
-
-const name = "laps"
-const age = 40
-
-if(age > 0 && age <= 12){
-    console.log("laps")
-}else if(age > 12 && age < 18){
-    console.log("teen")
-}else{
-    console.log("ok")
-}
-
-let id = 5
-if(id === 100){
-    console.log("OK")
-}else{
-    console.log("not")
-}
-
-console.log(id === 100 ? "ok" : "not")
-
-//switch
-const color = "yellow"
-
-switch (color) {
-    case "red":
-        console.log("stop")
-        break;
-    case " yellow":
-        console.log("Attention")
-        break;
-    default:
-        console.log("whatever")
-}
