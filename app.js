36f308f7a2fa821d95357bf702f87d8d76d915cf
Kristjan-Even <kristjan.puusepp@voco.ee>
2022-09-22 13:44:21 +0300

    if, else and switch

--- app.js
+++ app.js
@@ -1,21 +1,80 @@
-const today = new Date()
+/*
+if(condition) {
+    //if statements
+} else{
+    // else statements
+}
 
-let val
+== equal ===
+!= not equal !==
 
-val = today
-val = today.getMonth() // 0 = jaanuar
-val = today.getDay()
-val = today.getDate()
-val = today.getFullYear()
-val = today.getHours()
-val = today.getSeconds()
-val = today.getTime()
 
-val = new Date("09/22/21")
-val = new Date("September 23 2022")
 
-val = new Date()
-val.setDate(23)
-val.setFullYear(2023)
+ */
 
-console.log(val)
+// let person_id = 34;
+//
+// if(person_id === "undefined"){
+//     console.log("id pähh pähh")
+// } else{
+//     console.log(`id is $(person_id)`)
+// }
+
+/*
+if(condition) {
+    //if statements
+} else{
+    // else statements
+}
+
+
+ */
+
+
+// const color = "red"
+//
+// if(color === "red") {
+//     console.log("go")
+//
+// }else if (color === "yellow") {
+//     console.log("stop")
+// } else {
+//     console.log("noob")
+// }
+//
+// console.log(person_id)
+
+
+const name = "laps"
+const age = 40
+
+if(age > 0 && age <= 12){
+    console.log("laps")
+}else if(age > 12 && age < 18){
+    console.log("teen")
+}else{
+    console.log("ok")
+}
+
+let id = 5
+if(id === 100){
+    console.log("OK")
+}else{
+    console.log("not")
+}
+
+console.log(id === 100 ? "ok" : "not")
+
+//switch
+const color = "yellow"
+
+switch (color) {
+    case "red":
+        console.log("stop")
+        break;
+    case " yellow":
+        console.log("Attention")
+        break;
+    default:
+        console.log("whatever")
+}
