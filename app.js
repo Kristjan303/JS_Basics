ab4f133c70239032d40e2e54c3812c6e1b4ca912
Kristjan-Even <kristjan.puusepp@voco.ee>
2022-09-22 13:17:36 +0300

    time procressing

--- app.js
+++ app.js
@@ -1,42 +1,21 @@
-//object
-
-const person = {
-    firstname: "Kristjan-Even",
-    surname: "Puusepp",
-    age: 17,
-    email: "Kristjan.puusepp@voco.ee",
-    hobbies: ["Tennis", "korvpall"],
-    address: {
-        city: "Tartu",
-        county: "tartumaa"
-    },
-    getyear: function (){
-        return 2022 - this.age
-    }
-}
-
-
+const today = new Date()
 
 let val
-val = person
-val = person.firstname
-val = person["surname"]
-val = person["hobbies"][1]
-val = person.address.county
 
-val = person.getyear()
+val = today
+val = today.getMonth() // 0 = jaanuar
+val = today.getDay()
+val = today.getDate()
+val = today.getFullYear()
+val = today.getHours()
+val = today.getSeconds()
+val = today.getTime()
+
+val = new Date("09/22/21")
+val = new Date("September 23 2022")
+
+val = new Date()
+val.setDate(23)
+val.setFullYear(2023)
 
 console.log(val)
-
-// for (let i = 0;  i <person.hobbies.length; i++){
-//     console.log(person.hobbies[1])
-//
-// }
-// person.hobbies.forEach(function (hobbies, index){
-//     console.log(hobbies + " on olemas indexiga " +
-//         "" + index)
-//     console.log(hobbies)
-// })
-person.hobbies.forEach((hobbie, index) =>{
-    console.log(hobbie + " on element indexiga " + index)
-})
